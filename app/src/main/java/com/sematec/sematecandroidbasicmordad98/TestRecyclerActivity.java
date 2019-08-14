980ddc0a0d7d0212223f69dfd69bd48958fc6508
Pouya <pouyaheydary@gmail.com>
2019-08-14 17:37:08 +0430

    end of session 6

---
+++ app/src/main/java/com/sematec/sematecandroidbasicmordad98/TestRecyclerActivity.java
@@ -0,0 +1,45 @@
+package com.sematec.sematecandroidbasicmordad98;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestRecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_recycler);
+
+
+        RecyclerView recycler = findViewById(R.id.recycler);
+
+        List<String> list = new ArrayList<>();
+        list.add("Pouya Heydari");
+        list.add("Fateme Afrashi");
+        list.add("Javad Barqamadi");
+        list.add("Mehri Jamshidi");
+        list.add("Ali Khakbaz");
+        list.add("Mahsa Zolqadri");
+        list.add("Yasaman Rafiee");
+        list.add("Kiarash Shadloo");
+        list.add("Sadaf Farhood");
+        list.add("Fateme Mohammadi");
+        list.add("Hanie Moenian");
+        list.add("Sepehr Kashani");
+        list.add("Yasaman Karimi");
+
+
+
+        TestRecyclerAdapter adapter = new TestRecyclerAdapter(list);
+        recycler.setAdapter(adapter);
+        recycler.setLayoutManager(new LinearLayoutManager(TestRecyclerActivity.this, RecyclerView.VERTICAL, false));
+
+
+    }
+}
---
+++ app/src/main/java/com/sematec/sematecandroidbasicmordad98/TestRecyclerAdapter.java
@@ -0,0 +1,49 @@
+package com.sematec.sematecandroidbasicmordad98;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TestRecyclerAdapter extends RecyclerView.Adapter<TestRecyclerAdapter.TestRecyclerViewHolder> {
+
+    List<String> myList;
+    public TestRecyclerAdapter(List<String> list) {
+        myList = list;
+    }
+
+    @NonNull
+    @Override
+    public TestRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.test_recycler_item, parent, false);
+        TestRecyclerViewHolder holder = new TestRecyclerViewHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TestRecyclerViewHolder holder, int position) {
+
+        String name = myList.get(position);
+        holder.txtName.setText(name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return myList.size();
+    }
+
+    class TestRecyclerViewHolder extends RecyclerView.ViewHolder {
+        TextView txtName;
+
+        public TestRecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtName = itemView.findViewById(R.id.txtName);
+        }
+    }
+}
